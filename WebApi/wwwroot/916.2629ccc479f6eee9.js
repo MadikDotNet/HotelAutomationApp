af9a76ad541d7358c7e8e9282b53be02f72af094
MadikDotNet <moldageldinmadik@gmail.com>
2021-12-13 01:02:59 +0600

    q

---
+++ WebApi/wwwroot/916.2629ccc479f6eee9.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkhotelFront=self.webpackChunkhotelFront||[]).push([[916],{6916:(a,l,o)=>{o.r(l),o.d(l,{AdminModule:()=>t});var d=o(6019),u=o(3668);let t=(()=>{class n{}return n.\u0275fac=function(m){return new(m||n)},n.\u0275mod=u.oAB({type:n}),n.\u0275inj=u.cJS({imports:[[d.ez]]}),n})()}}]);
