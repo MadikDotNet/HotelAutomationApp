f544e4a22dd257b03f310d37e391a9dd8a6e3243
MadikDotNet <moldageldinmadik@gmail.com>
2021-12-12 23:30:51 +0600

    q

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -46,6 +46,8 @@ namespace HotelAutomationApp.WebApi
             }
 
             appBuilder.UseHttpsRedirection();
+
+            appBuilder.UseStaticFiles();
             
             appBuilder.UseAuthenticationSystem();
 
