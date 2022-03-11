39662afa01b9060a2347876eee9faf8794989a11
MadikDotNet <moldageldinmadik@gmail.com>
2022-03-11 19:33:54 +0600

    fix

--- Application/MediaFiles/Models/MediaDto.cs
+++ Application/MediaFiles/Models/MediaDto.cs
@@ -4,8 +4,8 @@ namespace HotelAutomationApp.Application.MediaFiles.Models
 {
     public record MediaDto : BaseEntityDto
     {
-        public string FileName { get; set; }
-        public string FileType { get; set; }
-        public string Data { get; set; }
+        public string? FileName { get; set; }
+        public string? FileType { get; set; }
+        public string? Data { get; set; }
     }
 }
