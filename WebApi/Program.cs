a31e09d2c9c4748e89521e709634c9d1f6abe4fc
MadikDotNet <moldageldinmadik@gmail.com>
2022-04-17 18:13:22 +0600

    test

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -26,7 +26,7 @@ namespace HotelAutomationApp.WebApi
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>()
-                        .UseUrls("http://*:80");
+                        .UseUrls("http://*:80808");
                 });
     }
 }
