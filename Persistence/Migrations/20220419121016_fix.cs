849cd034f90d7da13e9112c2b63b88531379dd0d
MadikDotNet <moldageldinmadik@gmail.com>
2022-04-19 18:11:14 +0600

    fix

---
+++ Persistence/Migrations/20220419121016_fix.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelAutomationApp.Persistence.Migrations
+{
+    public partial class fix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Service_ServiceGroup_ServiceGroupId",
+                table: "Service");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ServiceGroupId",
+                table: "Service",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Service_ServiceGroup_ServiceGroupId",
+                table: "Service",
+                column: "ServiceGroupId",
+                principalTable: "ServiceGroup",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Service_ServiceGroup_ServiceGroupId",
+                table: "Service");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ServiceGroupId",
+                table: "Service",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Service_ServiceGroup_ServiceGroupId",
+                table: "Service",
+                column: "ServiceGroupId",
+                principalTable: "ServiceGroup",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
